0824ca0286d915f07ce523beccc3234cc359ae32
Rares Ioel Dragusin <dragusinrares@gmail.com>
2026-02-23 23:09:47 +0200

    update 2
    update 2 since full stack app uploaded

--- frontend/src/features/home/HomePage.jsx
+++ frontend/src/features/home/HomePage.jsx
@@ -10,7 +10,7 @@ export default function HomePage() {
     <div className="min-h-screen bg-slate-50">
       <div className="mx-auto max-w-3xl px-4 py-10">
         {/* Title */}
-        <h1 className="text-4xl font-extrabold text-slate-900">EventUp</h1>
+        <h1 className="text-4xl font-extrabold text-slate-900">Eventify</h1>
 
         {/* Authentication part */}
         <div className="mt-4">
